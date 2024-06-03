977de31347b82f9362221fd311864b5fe58f99e2
Gautam Adatra <gautamkiran123@gmail.com>
2024-06-03 20:50:14 +0530

    db logic added

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://gautamkiran123:dElw0UD5nYGFoKzD@cluster0.kkdxlin.mongodb.net/");
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: String
+})
