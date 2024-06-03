ca18083144c7dd2a2377b49c89d5b3ae06846219
Gautam Adatra <gautamkiran123@gmail.com>
2024-06-03 23:34:50 +0530

    routes logic

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { createTodo } = require("./types");
+const { todo } = require("./db");
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/todo', (req,res)=>{
+app.post('/todo', async (req,res)=>{
     const inputTodo = req.body;
     const parseTodo = createTodo.safeParse(inputTodo);
     if(!parseTodo.success){
@@ -14,13 +15,25 @@ app.post('/todo', (req,res)=>{
         })
         return;
     }
+    await todo.create({
+        title: parseTodo.title,
+        description: parseTodo.description,
+        completed: false
+    })
+    res.json({
+        msg: "todo created"
+    })
     
 });
-app.get('/todos', (req,res)=>{
+app.get('/todos', async (req,res)=>{
+    const todos = await todo.find();
 
+    res.json({
+        todos,
+    })
 });
 
-app.put('/completed', (req,res)=>{
+app.put('/completed', async (req,res)=>{
     const updateTodo = req.body;
     const parseUpdate = updateTodo.safeParse(updateTodo);
     if(!parseUpdate.success){
@@ -29,6 +42,10 @@ app.put('/completed', (req,res)=>{
         })
         return;
     }
+    await todo.update({_id: req.body.id}, {completed:true});
+    res.json({
+        msg: "Todo marked as completed"
+    })
 });
 app.listen(3000, ()=>{
     console.log("port is listening");
